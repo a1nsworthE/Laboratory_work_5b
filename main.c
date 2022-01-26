ead957df69dd8ce79fcda902ce6b8169f96c095d
Daniel Wahbi <xed255@gmail.com>
2022-01-26 15:43:28 +0300

    Добавлено решение задачи 2 из второго блока

--- main.c
+++ main.c
@@ -3,8 +3,7 @@
 #include <stdlib.h>
 #include "includes/algorithms/matrix.h"
 
-int main() {
-    size_t n;
+/*  size_t n;
     scanf("%zd", &n);
 
     matrix m = getMemMatrix(n, n);
@@ -15,6 +14,31 @@ int main() {
     swapRows(m, indexMin, indexMax);
 
     outputMatrix(m);
+    freeMemMatrix(matrix1);
+*/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+int main() {
+    size_t n, m;
+    scanf("%zd %zd", &n, &m);
+    matrix matrix1 = getMemMatrix(n, m);
+    inputMatrix(matrix1);
+    insertionSortRowsMatrixByRowCriteria(matrix1, getMaxElemArray);
+
+    outputMatrix(matrix1);
+    freeMemMatrix(matrix1);
 
     return 0;
 }
