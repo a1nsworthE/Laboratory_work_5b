2c5c849d560e828366d7a8adaac54c01214b03a1
Daniel Wahbi <xed255@gmail.com>
2022-01-26 17:10:02 +0300

    добавленны комментарии

--- main.c
+++ main.c
@@ -3,29 +3,56 @@
 #include <stdlib.h>
 #include "includes/algorithms/matrix.h"
 
+void sortRowsByMinElement(matrix m) {
+    insertionSortRowsMatrixByRowCriteria(m, getMaxElemArray);
+}
+
+void sortColsByMinElement(matrix m) {
+    insertionSortColsMatrixByColCriteria(m, getMinElemArray);
+}
+// 1 задача>>>
 /*  size_t n;
     scanf("%zd", &n);
 
     matrix m = getMemMatrix(n, n);
     inputMatrix(m);
-
     size_t indexMin = getMinValuePos(m).rowIndex;
     size_t indexMax = getMaxValuePos(m).rowIndex;
     swapRows(m, indexMin, indexMax);
 
     outputMatrix(m);
     freeMemMatrix(matrix1);
+
+    return 0;
 */
-/* size_t n, m;
+
+// 2 задача>>>
+/*  size_t n, m;
     scanf("%zd %zd", &n, &m);
     matrix matrix1 = getMemMatrix(n, m);
     inputMatrix(matrix1);
-    insertionSortRowsMatrixByRowCriteria(matrix1, getMaxElemArray);
+    sortRowsByMinElement(matrix1);
 
     outputMatrix(matrix1);
     freeMemMatrix(matrix1);
- * */
-/**/
+
+    return 0;
+*/
+
+// 3 задача>>>
+/*  size_t n, m;
+    scanf("%zd %zd", &n, &m);
+    matrix matrix1 = getMemMatrix(n, m);
+    inputMatrix(matrix1);
+    sortColsByMinElement(matrix1);
+
+    outputMatrix(matrix1);
+    freeMemMatrix(matrix1);
+
+    return 0;
+*/
+
+// 4 задача>>>
 /**/
 /**/
 /**/
@@ -43,7 +70,6 @@ int main() {
     scanf("%zd %zd", &n, &m);
     matrix matrix1 = getMemMatrix(n, m);
     inputMatrix(matrix1);
-    insertionSortColsMatrixByColCriteria(matrix1, getMinElemArray);
 
     outputMatrix(matrix1);
     freeMemMatrix(matrix1);
