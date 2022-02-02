253a2ca37fd54d76ebd4a389d7c17ca1b0e71703
Daniel Wahbi <xed255@gmail.com>
2022-02-03 00:17:35 +0300

    Добавление структуры matrixF

--- includes/structs/matrix.c
+++ includes/structs/matrix.c
@@ -6,6 +6,13 @@ typedef struct matrix {
     int nCols;     // кол - во столбцов
 } matrix;
 
+typedef struct matrixF {
+    float **values; // элементы матриц
+    int nRows;      // кол - во строк
+    int nCols;      // кол - во столбцов
+} matrixF;
+
+
 typedef struct position {
     size_t rowIndex;
     size_t colIndex;
